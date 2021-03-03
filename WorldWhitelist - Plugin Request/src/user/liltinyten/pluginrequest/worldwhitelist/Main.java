6829322f1ba02aef997a5b92f75eb6f74b6c8dcb
liltinyten <liltinyten@gmail.com>
2021-03-03 13:10:55 -0600

    Add files via upload

---
+++ WorldWhitelist - Plugin Request/src/user/liltinyten/pluginrequest/worldwhitelist/Main.java
@@ -0,0 +1,31 @@
+package user.liltinyten.pluginrequest.worldwhitelist;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin {
+	private static FileConfiguration config = null;
+	public static Main mainclass;
+	
+	@Override
+	public void onEnable() {
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN+"WorldWhitelist has been enabled!");
+		this.getCommand("wwhitelist").setExecutor(new whitelister());
+		getServer().getPluginManager().registerEvents(new worldListener(), this);
+		config = this.getConfig();
+		this.saveDefaultConfig();
+		mainclass = this;
+	}
+	
+	public static FileConfiguration getConfigFile(){
+		return config;
+	}
+	
+	@Override
+	public void onDisable() {
+		getServer().getConsoleSender().sendMessage(ChatColor.RED+"WorldWhitelist has been disbaled!");
+	}
+
+}
---
+++ WorldWhitelist - Plugin Request/src/user/liltinyten/pluginrequest/worldwhitelist/whitelister.java
@@ -0,0 +1,139 @@
+package user.liltinyten.pluginrequest.worldwhitelist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class whitelister implements CommandExecutor {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length > 0) {
+			// for on and off
+			if (args.length == 2) {
+				if (args[0].equalsIgnoreCase("on")) {
+					// toggle on code here
+					if (sender.hasPermission("wwhitelist.on")) {
+						// checks if it's /wwhitelist on
+						if (Main.getConfigFile().contains(args[1] + ".enabled")) {
+							if (Main.getConfigFile().getBoolean(args[1] + ".enabled") == false) {
+								sender.sendMessage(ChatColor.GREEN +"Enabled WorldWhitelist for " + args[1]);
+								Main.getConfigFile().set((args[1] + ".enabled"), true);
+								Main.mainclass.saveConfig();
+							} else {
+								sender.sendMessage(ChatColor.RED +"WorldWhitelist is already enabled for this world!");
+							}
+						} else {
+							sender.sendMessage(ChatColor.GREEN +"Enabled WorldWhitelist for " + args[1]);
+							Main.getConfigFile().set((args[1] + ".enabled"), true);
+							Main.mainclass.saveConfig();
+						}
+					} else {
+						sender.sendMessage(ChatColor.RED + "No permission!");
+					}
+				}
+			}
+				
+				
+				if (args[0].equalsIgnoreCase("off")) {
+					// toggle off code here
+					if (sender.hasPermission("wwhitelist.off")) {
+						// checks if it's /wwhitelist off
+						if (Main.getConfigFile().contains(args[1] + ".enabled")) {
+						if (Main.getConfigFile().getBoolean(args[1] + ".enabled") == true) {
+							sender.sendMessage(ChatColor.GREEN +"Disabled WorldWhitelist for " + args[1]);
+							Main.getConfigFile().set((args[1] + ".enabled"), false);
+							Main.mainclass.saveConfig();
+						} else {
+							sender.sendMessage(ChatColor.RED +"WorldWhitelist is already disabled for this world!");
+						}
+					} else {
+						sender.sendMessage(ChatColor.GREEN +"Disabled WorldWhitelist for " + args[1]);
+						Main.getConfigFile().set((args[1] + ".enabled"), false);
+						Main.mainclass.saveConfig();
+					}
+				  } else {
+					  sender.sendMessage(ChatColor.RED + "No permission!");
+				  }
+				
+
+				
+			}
+			
+			if (args[0].equalsIgnoreCase("help")) {
+				sender.sendMessage(ChatColor.YELLOW+"/wwhitelist add/remove player world - Allows you to add and remove players from a world whitelist" + "\n" + "/wwhitelist on/off world - Toggles WorldWhitelist for a world");
+			}
+			
+			// for add and remove
+			if (args.length == 3) {
+				String worldname = args[2];
+				String playername = args[1];
+				
+				if (args[0].equalsIgnoreCase("add")) {
+					// add code here
+					if (sender.hasPermission("wwhitelist.add")) {
+						if (Main.getConfigFile().contains(worldname + ".players")) {
+							List<String> players = Main.getConfigFile().getStringList(worldname + ".players"); 
+							OfflinePlayer player = Bukkit.getOfflinePlayer(playername);
+							String PlayerID = player.getUniqueId().toString();
+							if (players.contains(PlayerID)) {
+								sender.sendMessage(ChatColor.RED +playername + " is already on the whitelist!");
+							} else {
+								players.add(PlayerID);
+								Main.getConfigFile().set(worldname + ".players", players);
+								sender.sendMessage(ChatColor.GREEN +"Added " + playername + " to the whitelist!");
+								Main.mainclass.saveConfig();
+							}
+
+						} else {
+							List<String> oneplayer = new ArrayList<String>();
+							OfflinePlayer player = Bukkit.getOfflinePlayer(playername);
+							String PlayerID = player.getUniqueId().toString();
+							oneplayer.add(PlayerID);
+							Main.getConfigFile().set(worldname + ".players", oneplayer);
+							sender.sendMessage(ChatColor.GREEN +"Added " + playername + " to the whitelist!");
+							Main.mainclass.saveConfig();
+						}
+					} else {
+						sender.sendMessage(ChatColor.RED+"No permission!");
+					}
+					
+				}
+				if (args[0].equalsIgnoreCase("remove")) {
+					// remove code here
+					if (sender.hasPermission("wwhitelist.remove")) {
+						if (Main.getConfigFile().contains(worldname + ".players")) {
+							List<String> players = Main.getConfigFile().getStringList(worldname + ".players"); 
+							OfflinePlayer player = Bukkit.getOfflinePlayer(playername);
+							String PlayerID = player.getUniqueId().toString();
+							if (players.contains(PlayerID)) {
+								players.remove(PlayerID);
+								Main.getConfigFile().set(worldname + ".players", players);
+								sender.sendMessage(ChatColor.GREEN +"Removed " + playername + " from the whitelist!");
+								Main.mainclass.saveConfig();
+							} else {
+								sender.sendMessage(ChatColor.RED + playername + " is not in the whitelist!");
+							}
+						} else {
+							sender.sendMessage(ChatColor.RED +playername + " is not in the whitelist!");
+						}
+					} else {
+						sender.sendMessage(ChatColor.RED + "No permission!");
+					}
+				}
+				
+			}
+		}
+		
+		
+		return false;
+	}
+
+}
