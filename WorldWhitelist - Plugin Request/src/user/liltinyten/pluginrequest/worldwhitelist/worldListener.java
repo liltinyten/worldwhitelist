2fb3dc0f3338cb23d15b05a13ef17cd4df4dff5b
liltinyten <liltinyten@gmail.com>
2022-03-27 13:18:43 -0500

    Game version update
    Switched native version from 1.16 to 1.8 to potentially support all versions. If there are any issues please inform me.

--- WorldWhitelist - Plugin Request/src/user/liltinyten/pluginrequest/worldwhitelist/worldListener.java
+++ WorldWhitelist - Plugin Request/src/user/liltinyten/pluginrequest/worldwhitelist/worldListener.java
@@ -10,31 +10,32 @@ public class worldListener implements Listener {
 
 	@EventHandler
 	public void onWorld(PlayerChangedWorldEvent e) {
-		
-		// Checks if world whitelist is enabled for that world.
+
 		if (Main.getConfigFile().contains(e.getPlayer().getWorld().getName() + ".enabled")) {
 			if (Main.getConfigFile().getBoolean(e.getPlayer().getWorld().getName() + ".enabled") == true) {
 				String worldname = e.getPlayer().getWorld().getName();
-				
-				// Checks the player's permission for the world and checks if the world's configuration has a player list.
-				if ((Main.getConfigFile().contains(e.getPlayer().getWorld().getName() + ".players") || e.getPlayer().hasPermission("wwhitelist.bypass") || e.getPlayer().hasPermission("wwhitelist." + worldname) )) {
-					// Checks if the player is allowed.
-					if (!(Main.getConfigFile().getStringList(e.getPlayer().getWorld().getName() + ".players").contains(e.getPlayer().getUniqueId().toString()) || e.getPlayer().hasPermission("wwhitelist.bypass") || e.getPlayer().isOp())) {
-						// Returns the player to their original world.
+
+
+				if ((Main.getConfigFile().contains(e.getPlayer().getWorld().getName() + ".players"))) {
+					if (!(Main.getConfigFile().getStringList(e.getPlayer().getWorld().getName() + ".players").contains(e.getPlayer().getUniqueId().toString()) 
+							|| e.getPlayer().hasPermission("wwhitelist.bypass") 
+							|| e.getPlayer().isOp()
+							|| e.getPlayer().hasPermission("wwhitelist." + worldname)
+							)) {
+
 						e.getPlayer().teleport(e.getFrom().getSpawnLocation());
 						e.getPlayer().sendMessage(ChatColor.RED + "You are not whitelisted on this world!");
 					}
-				
-				   } else {
-					   // Returns the player to their original world.
-					   e.getPlayer().teleport(e.getFrom().getSpawnLocation());
-					   e.getPlayer().sendMessage(ChatColor.RED + "You are not whitelisted on this world!");
-				   }
+
+				} else {
+					e.getPlayer().teleport(e.getFrom().getSpawnLocation());
+					e.getPlayer().sendMessage(ChatColor.RED + "You are not whitelisted on this world!");
+				}
 			}
 		}
-			
-		
-		
-		
+
+
+
+
 	}
 }
